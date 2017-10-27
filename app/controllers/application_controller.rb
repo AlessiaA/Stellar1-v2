e279d98ebc7c77bd473aa647e88e0f656259eda9
AlessiaA <alessiaantonutti@gmail.com>
2017-10-27 21:43:23 +0200

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,60 @@
+#
+# Classe ereditata da tutti i controller.
+#
+class ApplicationController < ActionController::Base
+
+  layout :set_layout
+  protect_from_forgery with: :exception
+  # Prima di poter accedere alle funzionalità della pagina, è richiesta
+  # l'autenticazione dell'utente.
+  before_action :authenticate_user!
+
+  # Questo metodo invoca i metodi richiesti per la visualizzazione della
+  # pagina iniziale, dipendentemente dal ruolo dell'utente.
+  def index
+    if current_user.has_role? :admin
+      admin_index
+    else
+      user_index
+    end
+  end
+
+  private
+
+  # Metodo che visualizza la pagina iniziale degli utenti amministratori.
+  def admin_index
+    # Esegue la ricerca degli utenti.
+    @q = ObservativeSession.ransack(params[:q])
+    @observative_sessions = @q.result.order(params[:order]).paginate(page: params[:page]) if params[:q].present?
+    @observative_sessions = ObservativeSession.order(params[:orser]).paginate(page: params[:page]) unless params[:q].present?
+    @observative_session = ObservativeSession.new
+	# visualizza la lista degli utenti iscritti.
+    render 'observative_sessions/index'
+  end
+
+  # Metodo che visualizza la pagina iniziale degli utenti ordinari.
+  def user_index
+	@q = ObservativeSession.ransack(params[:q])
+    @observative_sessions = @q.result.order(params[:order]).paginate(page: params[:page]) if params[:q].present?
+    @observative_sessions = ObservativeSession.order(params[:orser]).paginate(page: params[:page]) unless params[:q].present?
+    @observative_session = ObservativeSession.new
+    # visualizza la pagina iniziale degli utenti ordinari.
+	render 'observative_sessions/index'
+  end
+
+  # Metodo per impostare il layout della pagina, sulla base
+  # del ruolo dell'utente.
+  def set_layout
+    if current_user
+      case current_user.role
+      when "admin"
+        "admin"
+      when "user"
+        "user"
+      end
+    else
+      "default"
+    end
+  end
+
+end
